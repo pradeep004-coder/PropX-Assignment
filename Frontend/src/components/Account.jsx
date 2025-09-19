18923b8457029940631edde597b198b674bb760c
pradeep <pp0931275@gmail.com>
2025-09-19 16:25:32 +0530

    /account not found resolved

--- Frontend/src/components/Account.jsx
+++ Frontend/src/components/Account.jsx
@@ -1,14 +1,21 @@
 import { useContext, useEffect } from 'react';
 import { userContext } from '../context/context';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router';
 
 function Account() {
 
     const { userData } = useContext(userContext);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        if (!userData.name || !userData.email) return toast.error("Please login or signup!");
-    }, []);
+        if (!userData?.name || !userData?.email) {
+             toast.error("Please login or signup!");
+             setTimeout(() => {
+                navigate("/");
+             }, 2000);
+        }
+    }, [userData]);
 
     return (
         <div className='h-screen w-full flex justify-center'>
@@ -19,14 +26,14 @@ function Account() {
                     <div className='h-fit px-4 pb-4 border-b border-dashed'>
                         <div className="flex gap-4 mt-4">
                             <div className="profile-image">
-                                <img alt="Profile" src="./images/profile-icon.png" className='w-20' />
+                                <img alt="Profile" src="/images/profile-icon.png" className='w-20' />
                                 <div className='w-fit bg-purple-800 p-1 rounded-full relative -top-6 left-16'>
-                                    <img alt="Camera Icon" src="./images/camera-icon.png" className='w-3 invert' />
+                                    <img alt="Camera Icon" src="/images/camera-icon.png" className='w-3 invert' />
                                 </div>
                             </div>
                             <div className="profile-info" >
-                                <h3 className='font-bold'>{userData.name || "Name"}</h3>
-                                <p>{userData.email || "email@domain.com"}</p>
+                                <h3 className='font-bold'>{userData?.name || "Name"}</h3>
+                                <p>{userData?.email || "email@domain.com"}</p>
                             </div>
                         </div>
                         <p >
