80e6fd3f69322d8865eff4566ab5c558ad616974
pradeep <pp0931275@gmail.com>
2025-09-19 16:14:07 +0530

    /account not found resolved

--- Frontend/src/context/context.jsx
+++ Frontend/src/context/context.jsx
@@ -3,7 +3,7 @@ import { useState, createContext } from "react";
 const userContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState({name: "", email: ""});
 
   return <userContext.Provider value={{ userData, setUserData }}>
     {children}
